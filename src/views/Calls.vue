27511845bd9db33c05709a599b8b060d5496c736
Lijuancheng <pushuhexi11@hotmail.com>
2020-10-30 14:47:35 +0100

    style for desktop

--- src/views/Calls.vue
+++ src/views/Calls.vue
@@ -1,31 +1,30 @@
 <template>
-<div class="wrapper">
+  <div class="wrapper">
     <Header />
-  <div class="call-page">
-   
-    <h1>Call</h1>
-    <section class="calls">
-      <div class="call-one">
-        <img src="../assets/missed-call.png" alt="missed call" />
-        <span class="name">Akinyi Matata</span>
-        <span class="time">15:30,2020-10-13</span>
-        <button class="button-small">Call</button>
-      </div>
-      <div class="call-one">
-        <img src="../assets/call-received.png" alt="received call" />
-        <span class="name">Akinyi Matata</span>
-        <span class="time">14:50,2020-10-13</span>
-        <button class="button-small">Call</button>
-      </div>
-      <div class="call-one">
-        <img src="../assets/call-outgoing.png" alt="call-outgoing" />
-        <span class="name">Nia Shwalli</span>
-        <span class="time">11:20,2020-10-12</span>
-        <button class="button-small">Call</button>
-      </div>
-    </section>
-</div>
-  <Footer />
+    <div class="call-page">
+      <h1>Calls</h1>
+      <section class="calls">
+        <div class="call-one">
+          <img src="../assets/missed-call.png" alt="missed call" />
+          <span class="name">Akinyi Matata</span>
+          <span class="time">15:30,2020-10-13</span>
+          <button class="button-small">Call</button>
+        </div>
+        <div class="call-one">
+          <img src="../assets/call-received.png" alt="received call" />
+          <span class="name">Akinyi Matata</span>
+          <span class="time">14:50,2020-10-13</span>
+          <button class="button-small">Call</button>
+        </div>
+        <div class="call-one">
+          <img src="../assets/call-outgoing.png" alt="call-outgoing" />
+          <span class="name">Nia Shwalli</span>
+          <span class="time">11:20,2020-10-12</span>
+          <button class="button-small">Call</button>
+        </div>
+      </section>
+    </div>
+    <Footer />
   </div>
 </template>
 
@@ -36,8 +35,7 @@ import Footer from "@/components/Footer.vue";
 export default {
   components: {
     Header,
-    Footer
-   
+    Footer,
   },
 };
 </script>
@@ -53,36 +51,37 @@ export default {
     margin: auto
 
     .calls
-       height: 70vh
-       background: base.$light-grey
-       display: flex
-       flex-direction: column
-       width:100%
-       font-family: base.$font-Roboto
-       font-size: 14px
-      
-       .call-one
-           background: base.$white  
-           margin: 20px
-           height: 40px
-           border-radius: 8px
-           display: flex
-           flex-direction: row
-           justify-content: space-around
-           font-family: base.$font-Roboto
-           align-items: center
-           img 
-             width: 18px
-             height: 18px
-           .name
-               font-size: 14px
-           .time
-               font-size: 10px
-               
-               
+        height: 80vh
+        padding-top: 2em
+        background: base.$light-grey
+        display: flex
+        flex-direction: column
+        width: 100%
+        font-family: base.$font-Roboto
+        font-size: 14px
+
+        .call-one
+            background: base.$white
+            margin: 20px
+            height: 40px
+            border-radius: 8px
+            display: flex
+            flex-direction: row
+            justify-content: space-around
+            font-family: base.$font-Roboto
+            align-items: center
+           
+            img
+                width: 18px
+                height: 18px
+            .name
+                font-size: 14px
+            .time
+                font-size: 10px
+                margin-left: 2em
+
 .button-small
     +base.common-button-orange()
-    //margin: 20px
     width: 40px
     height: 21px
     cursor: pointer
@@ -91,26 +90,33 @@ export default {
     font-size: 12px
     &:focus
         outline: none
-h1
-    font-family: base.$font-Alfa
-    color: base.$black
-    font-size: 24px
-    letter-spacing: 2px
 
+  
 @media screen and (min-width: base.$breakpoint)
     h1
-       font-size: 36px
-   
-    .call-page .calls .call-one 
+        font-size: 36px
+
+    .call-page .calls .call-one
         width: 70%
         align-self: center
-        img 
-            width: 3% 
-            height: 3%
+        height: 60px
+        img
+            width: 32px
+            height: 24px
 
         .name
             font-size: 18px
         .time
-            font-size: 14px  
-    
+            font-size: 14px
+            margin-right:  25em
+        
+    .button-small
+        width: 58px
+        height: 32px
+        font-size: 18px
+        &:hover
+            border: 3px solid base.$red
+
+        
+        
 </style>
